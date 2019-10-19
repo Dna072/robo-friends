55b40b2182b89eff5cdea8e91bb250820391ffb2
dna072 <dnadjei07@hotmail.co.uk>
2019-10-19 22:56:49 +0000

    Updates

---
+++ precache-manifest.f32321de63d05138156e14af60a6551c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f125ce3d6072c7b796e4f2bf736dae28",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "5f1af6d57eda7fcc831a",
+    "url": "/robo-friends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b8dc0c6e27f935c9e006",
+    "url": "/robo-friends/static/css/main.6314db35.chunk.css"
+  },
+  {
+    "revision": "5f1af6d57eda7fcc831a",
+    "url": "/robo-friends/static/js/2.2d80292b.chunk.js"
+  },
+  {
+    "revision": "b8dc0c6e27f935c9e006",
+    "url": "/robo-friends/static/js/main.fc070b88.chunk.js"
+  },
+  {
+    "revision": "e26fa2088cf320dc503e",
+    "url": "/robo-friends/static/js/runtime~main.04d6e5ad.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
